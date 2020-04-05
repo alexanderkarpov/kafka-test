9c0f18476024ea76eb84f36ab9def01fd210fc47
akarpov <alexander.karpoff@gmail.com>
2020-04-05 12:36:10 +0300

    create producer

--- src/main/java/org/example/kafka/ProducerDemo.java
+++ src/main/java/org/example/kafka/ProducerDemo.java
@@ -1,26 +1,34 @@
 package org.example.kafka;
 
 
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.clients.producer.ProducerRecord;
 import org.apache.kafka.common.serialization.StringSerializer;
 
 import java.util.Properties;
 
 public class ProducerDemo {
 
-    public static void main(String ... args) {
+    public static void main(String... args) {
         String bootstrapServers = "127.0.0.1:9092";
 
         // create Producer properties
         Properties properties = new Properties();
         // see: https://kafka.apache.org/documentation/#producerconfigs
-        properties.setProperty("bootstap.servers", bootstrapServers);
-        properties.setProperty("key.serializer", StringSerializer.class.getName());
-        properties.setProperty("value.serializer", StringSerializer.class.getName());
-
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
 
         // create the Producer
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
 
-        // send data
+        // create a producer record
+        ProducerRecord<String, String> record = new ProducerRecord<>("first_topic", "hello world");
 
+        // send data (async)
+        producer.send(record);
+        producer.flush();
+        producer.close();
     }
 }
